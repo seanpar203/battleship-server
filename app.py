1b99cee454cf80aef53abb2a612e283fdd9f8ad9
Sean Parsons <sean@labelinteractive.com>
2016-08-21 20:08:17 -0400

    Added another route for testing.

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask
 
-APP = Flask(__name__)
+app = Flask(__name__)
 
 
-@APP.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/')
+def hello():
+	return "Hello World!"
+
+
+@app.route('/<name>')
+def hello_name(name):
+	return "Hello {}!".format(name)
 
 
 if __name__ == '__main__':
-    APP.run()
+	app.run()
