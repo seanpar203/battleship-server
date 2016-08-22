b5bfb22faf994b008955d2ee5af93b40117c2b8f
Sean Parsons <seanpatrick2013@gmail.com>
2016-08-22 15:33:13 -0400

    Added config to app.py to allow different settings.

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+""" Module that handles all the routes for the web-time-analytics extension."""
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 
 @app.route('/')
