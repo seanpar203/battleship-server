3d8b0857c9c4efd6f9915b164d34b483638cae3f
Sean Parsons <seanpatrick2013@gmail.com>
2016-08-24 12:45:15 -0400

    Set up Flask & SQLAlchemy Basic Config.

--- app.py
+++ app.py
@@ -2,20 +2,24 @@
 import os
 
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
+# Flask Config
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def hello():
-	return "Hello World!"
+    return "Hello World!"
 
 
 @app.route('/<name>')
 def hello_name(name):
-	return "Hello {}!".format(name)
+    return "Hello {}!".format(name)
 
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
