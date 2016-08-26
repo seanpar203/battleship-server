1a3742176f0771bcdf1039a72a6b69bfbcab77b5
Sean Parsons <seanpatrick2013@gmail.com>
2016-08-26 13:47:21 -0400

    Added boilerplate testing config

--- test.py
+++ test.py
@@ -1,10 +1,30 @@
-from fl
+""" Testing for Web Time Analytics app.
+
+This module, replicates & tests the functionality of the
+flask server and it's routes.
+"""
+
 import unittest
 
+from flask_testing import TestCase
 
-class MyTestCase(unittest.TestCase):
-	def test_something(self):
-		self.assertEqual(True, False)
+from app import app, db, User, TimeSpent
+from config import TestConfig
+
+
+class MyTestCase(TestCase):
+	SQLALCHEMY_DATABASE_URI = "sqlite://"
+
+	def create_app(self):
+		app.config.from_object(TestConfig)
+		return app(self)
+
+	def setUp(self):
+		db.create_all()
+
+	def tearDown(self):
+		db.session.remove()
+		db.drop_all()
 
 
 if __name__ == '__main__':
