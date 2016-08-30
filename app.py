4ac2e1eda106e2fec6eef69e61345564dd9bbcda
Sean Parsons <seanpatrick2013@gmail.com>
2016-08-30 15:46:45 -0400

    registered blueprint from user views.

--- app.py
+++ app.py
@@ -3,12 +3,18 @@ import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from user.views import users
 
-# Flask Config
+# Flask Config.
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Create DB Connection.
 db = SQLAlchemy(app)
+
+# Register blueprints.
+app.register_blueprint(users)
 
 # Import Models
 from user.models import UserToken
