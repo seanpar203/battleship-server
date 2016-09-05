f5d70ea33b1817065dc60518e299ecd2f9418ea5
Sean Parsons <seanpatrick2013@gmail.com>
2016-09-05 17:33:19 -0400

    Added view to save users time spent on web.

--- wta_app/time_spent/views.py
+++ wta_app/time_spent/views.py
@@ -1,11 +1,13 @@
 from wta_app.models import UserToken, TimeSpent, HostName
 from flask import Blueprint, request, jsonify
+from flask_cors import cross_origin
 from wta_app import db
 
-times = Blueprint('time', __name__, url_prefix='api/')
+times = Blueprint('time', __name__, url_prefix='/api')
 
 
-@times.route('time/', methods=(['POST']))
+@times.route('/time/', methods=(['POST']))
+@cross_origin()
 def create_time():
 	if 'token' in request.json:
 		req = request.json
