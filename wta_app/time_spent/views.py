acc1bfc7e70087fb17f0d9e9fb4fc11bdede64d5
Sean Parsons <seanpatrick2013@gmail.com>
2016-09-05 12:52:01 -0400

    Create new migration & new views in time_spent for storing time.

---
+++ wta_app/time_spent/views.py
@@ -0,0 +1,23 @@
+from wta_app.models import UserToken, TimeSpent, HostName
+from flask import Blueprint, request, jsonify
+from wta_app import db
+
+times = Blueprint('time', __name__, url_prefix='api/')
+
+
+@times.route('time/', methods=(['POST']))
+def create_time():
+	if 'token' in request.json:
+		req = request.json
+		time = TimeSpent(req['seconds'])
+		host = HostName.get_or_create(req['host'])
+		user = UserToken.get_or_create(req['token'])
+		user.time_spent.append(time)
+		time.host.append(host)
+		db.session.add(user)
+		db.session.add(time)
+		db.session.add(host)
+		db.session.commit()
+		return jsonify({'success': True}), 200
+	else:
+		return jsonify({'success': False, 'error': 'No Token Value.'}), 400
