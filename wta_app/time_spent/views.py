420e9f8cab0986785063682a25d8537cd8f1fe7d
Sean Parsons <seanpatrick2013@gmail.com>
2016-09-05 20:39:12 -0400

    Added helpers file & Organized views.py.

--- wta_app/time_spent/views.py
+++ wta_app/time_spent/views.py
@@ -3,26 +3,19 @@ from flask import Blueprint, request, jsonify
 from flask_cors import cross_origin
 from wta_app import db
 
-BAD_REQUEST = 400
-OK_REQUEST = 200
-
 times = Blueprint('time', __name__, url_prefix='/api')
+
+OK_REQUEST = 200
+BAD_REQUEST = 400
 
 
 @times.route('/time/', methods=(['GET', 'POST']))
 @cross_origin()
 def create_time():
-	# if request.module == 'GET':
-	# 	if 'token' in request.json:
-	# 		req = request.json
-	# 		user = UserToken.get_or_create(req['token'])
-	# 	else:
-	# 		return jsonify({'success': False}), BAD_REQUEST
 	if request.method == 'POST':
 		if 'token' in request.json:
 			req = request.json
 			user = UserToken.get_or_create(req['token'])
-
 			time = TimeSpent(req['seconds'])
 			host = HostName.get_or_create(req['host'])
 			user.time_spent.append(time)
@@ -31,6 +24,6 @@ def create_time():
 			db.session.add(time)
 			db.session.add(host)
 			db.session.commit()
-			return jsonify({'success': True}), OK_REQUEST
-		else:
-			return jsonify({'success': False, 'error':   'No Token Value.'}), BAD_REQUEST
+		return jsonify({'success': True}), OK_REQUEST
+	else:
+		return jsonify({'success': False, 'error': 'no Token'}), BAD_REQUEST
