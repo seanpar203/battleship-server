d646db8e9c9212e48c74f983d36c7c01c71aea30
Sean Parsons <seanpatrick2013@gmail.com>
2016-09-10 15:37:27 -0400

    Updated test data to reflect new model names.

--- wta_app/tests/test_data.py
+++ wta_app/tests/test_data.py
@@ -9,9 +9,9 @@ TEST_USER_TOKEN = '123456'
 TEST_TIME_SPENT = 10
 TEST_HOST_NAME = 'facebook.com'
 TEST_DATA = {
-	'user': TEST_USER_TOKEN,
-	'time': TEST_TIME_SPENT,
-	'host': TEST_HOST_NAME
+	'time':    TEST_TIME_SPENT,
+	'host':    TEST_HOST_NAME,
+	'account': TEST_USER_TOKEN,
 }
 
 
@@ -22,25 +22,6 @@ def create_test_data(**kwargs):
 		The intention of this function is to be able to run
 		generic tests and pass in the data, model & a bool if
 		a return value is desired for further testing.
-
-	Args:
-		**kwargs: Value of data, model & return bool.
-
-	Keyword Args:
-		data (str|int): Value of the data to use with creating model.
-		model (object): Model to be created with data.
-		retrn (bool): True to return the new object. Defaults to False.
-
-
-	Examples:
-
-		from app import db
-		from user.models import UserToken
-		from tests.test_data import create_dummy_data, DUMMY_TEST_DATA
-
-		user = create_dummy_data(DUMMY_TEST_DATA['user'], UserToken, True)
-
-
 
 	Returns:
 		object: Value of newly created & inserted model.
